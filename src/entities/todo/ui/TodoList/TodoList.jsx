6e3b92af5e4630f5d684ebd7686a3b1c5ca42201
Nikita Golubev <nikitagolubev@MacBook-Pro-Nikita.local>
2026-07-23 23:42:02 +0300

    fsd immigration + add animation and aliases

---
+++ src/entities/todo/ui/TodoList/TodoList.jsx
@@ -0,0 +1,30 @@
+import { memo, useContext } from "react";
+import { TasksContext, TodoItem } from "@/entities/todo";
+
+import styles from "./TodoList.module.scss";
+
+const TodoList = () => {
+  const { tasks, filteredTasks } = useContext(TasksContext);
+
+  const hasTasks = tasks.length > 0;
+  const isEmptyFilteredTasks = filteredTasks?.length === 0;
+
+  if (!hasTasks) {
+    return <div className={styles.emptyMessage}>There are no tasks yet</div>;
+  }
+
+  if (hasTasks && isEmptyFilteredTasks) {
+    return <div className={styles.emptyMessage}>Tasks not found</div>;
+  }
+
+  return (
+    <ul className={styles.list}>
+      {(filteredTasks ?? tasks).map((task) => (
+        // <TodoItem className="todo__item" key={task.id} {...task} />
+        <TodoItem key={task.id} {...task} />
+      ))}
+    </ul>
+  );
+};
+
+export default memo(TodoList);
